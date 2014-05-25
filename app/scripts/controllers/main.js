d46621bede6fa0bd734b64ddee1d693adbf0cb46
Javier Cejudo <javier@javiercejudo.com>
2014-05-26 08:18:09 +1000

    Basic API demo

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -1,10 +1,32 @@
 'use strict';
 
 angular.module('dejavideo2App')
-  .controller('MainCtrl', function ($scope) {
+  .controller('MainCtrl', function ($scope, $http) {
     $scope.awesomeThings = [
       'HTML5 Boilerplate',
       'AngularJS',
       'Karma'
     ];
+
+    $scope.randomQuote = {
+        enabled: false,
+        author: null,
+        text: null
+    };
+
+    $scope.loadRandomQuote = function () {
+        var randomQuote = $scope.randomQuote;
+
+        $http({method: 'GET', url: '/api/quote/random'})
+            .success(function(data, status, headers, config) {
+                randomQuote.author = data.author;
+                randomQuote.text = data.text;
+                randomQuote.enabled = true;
+            })
+            .error(function(data, status, headers, config) {
+                randomQuote.enabled = false;
+            });
+    };
+
+    $scope.loadRandomQuote();
   });
--- web.js
+++ web.js
@@ -1,15 +1,31 @@
-// web.js
-var express = require("express");
-var logfmt = require("logfmt");
+var express = require('express');
 var app = express();
 
-app.use(logfmt.requestLogger());
+app.use(express.static(__dirname + '/app'));
 
-app.get('/', function(req, res) {
-  res.send('<img alt="Embedded Image" src="data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAADIA..." />');
+var API_URL = '/api';
+
+var quotes = [
+  { author : 'Audrey Hepburn', text : "Nothing is impossible, the word itself says 'I'm possible'!"},
+  { author : 'Walt Disney', text : "You may not realize it when it happens, but a kick in the teeth may be the best thing in the world for you"},
+  { author : 'Unknown', text : "Even the greatest was once a beginner. Don't be afraid to take that first step."},
+  { author : 'Neale Donald Walsch', text : "You are afraid to die, and you're afraid to live. What a way to exist."}
+];
+
+app.all(API_URL, function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "X-Requested-With");
+  next();
+ });
+
+app.get(API_URL, function(req, res, next) {
+    res.json(quotes);
 });
 
-var port = Number(process.env.PORT || 5000);
-app.listen(port, function() {
-  console.log("Listening on " + port);
+app.get(API_URL + '/quote/random', function(req, res, next) {
+  var id = Math.floor(Math.random() * quotes.length);
+  var q = quotes[id];
+  res.json(q);
 });
+
+app.listen(process.env.PORT || 8000);
